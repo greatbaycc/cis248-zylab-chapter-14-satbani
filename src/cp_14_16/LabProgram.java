b5498813610b07ba2c0da3fcc68e7260c81bac48
satbani <157083938+satbani@users.noreply.github.com>
2024-05-04 18:24:22 -0400

    Update LabProgram.java

--- src/cp_14_16/LabProgram.java
+++ src/cp_14_16/LabProgram.java
@@ -4,11 +4,25 @@ package cp_14_16;
 import java.util.Scanner;
 
 public class LabProgram {
-    /* Define your method here */
+   /* Define your method here */
+   public static double stepsToMiles(int steps) throws Exception {
+      if (steps < 0) {
+         throw new Exception("Exception: Negative step count entered.");
+      }
+      return (double) steps/2000;
+   }
+   public static void main(String[] args) {
+      Scanner scnr = new Scanner(System.in);
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        /* Type your code here. */
-    }
+      try {
+         int numSteps = scnr.nextInt();
+         double numMiles = stepsToMiles(numSteps);
+      
+         System.out.printf("%.2f", numMiles);
+         System.out.println();
+      }
+      catch (Exception e) {
+         System.out.println(e.getMessage());
+      }
+   }
 }
