d8bbbc82fe257852e45bc5c212f963e80661e208
satbani <157083938+satbani@users.noreply.github.com>
2024-05-04 18:14:10 -0400

    Update LabProgram.java

--- src/cp_14_15/LabProgram.java
+++ src/cp_14_15/LabProgram.java
@@ -1,23 +1,65 @@
-package cp_14_15;
-// Input errors with zyLabs
-
 import java.util.Scanner;
 import java.util.NoSuchElementException;
 
 public class LabProgram {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+   public static void main(String[] args) {
+      Scanner scnr = new Scanner(System.in);
 
-        int val1;
-        int val2;
-        int val3;
-        int max;
-
-        val1 = 0;
-        val2 = 0;
-        val3 = 0;
-
-        /* Type your code here. */
-
-    }
+      int val1;
+      int val2;
+      int val3;
+      int max;
+      
+      val1 = 0;
+      val2 = 0;
+      val3 = 0;
+      
+      int counter = 0;
+      
+      try {
+         val1 = scnr.nextInt();
+         counter++;
+         val2 = scnr.nextInt();
+         counter++;
+         val3 = scnr.nextInt();
+         counter++;
+         
+         if (val1 > val2) {
+            if (val1 > val3) {
+               max = val1;
+            }
+            else {
+               max = val3;
+            }
+         }
+         else {
+            if (val2 > val3) {
+               max = val2;
+            }
+            else {
+               max = val3;
+            }
+         }
+         System.out.println(max);
+      }
+      catch (NoSuchElementException e) {
+         System.out.println(counter + " input(s) read:");
+         if (counter > 1) {
+            if (val1 > val2) {
+               System.out.println("Max is " + val1);
+            }
+            else {
+               System.out.println("Max is " + val2);
+            }
+         }
+         else if (counter > 0) {
+            System.out.println("Max is " + val1);
+         }
+         else {
+            System.out.println("No max");
+         }
+      
+      }
+      
+   }
 }
