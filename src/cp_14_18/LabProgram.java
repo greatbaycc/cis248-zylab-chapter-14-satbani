f19ff2651a11c651c57dedf22a1c758fbb45437c
satbani <157083938+satbani@users.noreply.github.com>
2024-05-04 18:47:18 -0400

    Update LabProgram.java

--- src/cp_14_18/LabProgram.java
+++ src/cp_14_18/LabProgram.java
@@ -5,9 +5,21 @@ import java.util.Scanner;
 import java.util.InputMismatchException;
 
 public class LabProgram {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        /* Type your code here. */
-    }
+   public static void main(String[] args) {
+      Scanner scnr = new Scanner(System.in);
+      
+      try {
+         int userNum = scnr.nextInt();
+         int divNum = scnr.nextInt();
+         System.out.println(userNum / divNum);
+      }
+      catch (ArithmeticException e) {
+         System.out.println("Arithmetic Exception: " + e.getMessage());
+      }
+      catch (Exception e) {
+         System.out.println("Input Mismatch Exception: java.util.InputMismatchException");
+      }
+      
+      
+   }
 }
